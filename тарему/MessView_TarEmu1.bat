5d28dde5057e730e086e76d7bf2f663fe8d33d8d
Shchibrov <110598432+Shchibrov@users.noreply.github.com>
2023-03-11 16:45:05 +0300

    Add files via upload

---
+++ тарему/MessView_TarEmu1.bat
@@ -0,0 +1,27 @@
+messview.exe BINS_U-view.ini "RS-232 input message (measurement)" 1.dat 1.txt
+messview.exe BINS_U-view.ini "RS-232 input message (measurement)" 2.dat 2.txt
+messview.exe BINS_U-view.ini "RS-232 input message (measurement)" 3.dat 3.txt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ тарему/TarEmu1.bat
@@ -0,0 +1,2 @@
+TarEmu.exe =BINS_U.ini RS-232outputmessage(initialization).dat
+pause
