dc9d72e805db5e1deeac7ea78152c326f252a18a
Shchibrov <110598432+Shchibrov@users.noreply.github.com>
2023-03-12 14:48:08 +0300

    Add files via upload
    Добавлен запуск программы TarEmu

--- Sgs-main/WindowsFormsApp1/Form2.cs
+++ Sgs-main/WindowsFormsApp1/Form2.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.IO;
 using System.Threading;
 using System.Windows.Forms;
+using System.Windows.Automation;
 
 namespace WindowsFormsApp1
 {
@@ -35,15 +36,13 @@ namespace WindowsFormsApp1
 
             for (int i = 0; i < 3; i++)
             {
-                string programPath = @"C:\\Users\\1\\Desktop\\TarEmu1\\TarEmu1\\TarEmu.exe";
-                string programArgument = "";
-                // Запускаем приложение Блокнот
-                ProcessStartInfo startInfo = new ProcessStartInfo(programPath, programArgument);
-                Process process = new Process();
-                process.StartInfo = startInfo;
+                string tarEmuPath = @"D:\TarEmu\TarEmu.exe";
+                string args = @"=BINS_U.ini";
+                Process.Start(tarEmuPath, args);
+                SendKeys.Send("{F2}");
 
                 // Ждем 10 минуту
-                Thread.Sleep(600000);
+                Thread.Sleep(6000);
 
                 // Делаем скриншот и сохраняем его в файл в папке для скриншотов
                 SendKeys.Send("{PRTSC}");
@@ -52,7 +51,7 @@ namespace WindowsFormsApp1
                 screenshot.Save(filePath, System.Drawing.Imaging.ImageFormat.Png);
 
                 // Закрываем приложение Блокнот
-                Process[] processes = Process.GetProcessesByName("notepad");
+                Process[] processes = Process.GetProcessesByName("TarEmu");
                 for (int i1 = 0; i1 < processes.Length; i1++)
                 {
                     processes[i1].Kill();
